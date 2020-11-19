ff343cedc83ec6b20a24d0dcb25f5a8651ff12f3
Fire Dragon <daniwhitlock4@gmail.com>
2020-11-18 21:23:59 -0700

    added form submit button and text-area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 //Assign the button element object repressentation to a variable in the file
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //when the add a new task button is clicked, it will create a new task
-var createTasksHandler = function () {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
     var listItemEl = document.createElement("li"); //create a var for the list item
     listItemEl.className = "task-item"; //adding the style of the css .task-item class
     listItemEl.textContent = "This is a new task."; //what to type in
@@ -12,6 +14,7 @@ var createTasksHandler = function () {
 
 
 //On a button click, create a task using the createTaskHandler function
-buttonEl.addEventListener("click", createTasksHandler); 
+
+formEl.addEventListener("submit", createTaskHandler);
 
 
