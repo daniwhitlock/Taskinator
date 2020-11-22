62f285d5ecc2dbd855b18f74ea37209c1be349bb
Fire Dragon <daniwhitlock4@gmail.com>
2020-11-21 22:57:02 -0700

    finished code to reload on page when exit out--only thing left is to refractor

--- assets/js/script.js
+++ assets/js/script.js
@@ -201,7 +201,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
     saveTasks();
     
     
-;}
+};
 
 var taskStatusChangeHandler = function(event) {
     //get the task item's id
@@ -291,8 +291,57 @@ var dragLeaveHandler = function(event) {
 var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+//Add persistence and optimization
+// use localStorage to save tasks
+// refractor code
+var loadTasks = function () {
+    //Reassign the tasks variable to whatever localStorage returns.
+    
+    tasks = localStorage.getItem("tasks"); 
+    console.log(tasks);
+    if (tasks === null) {
+        tasks = [];
+        console.log(tasks);
+        return false; 
+    }
+    tasks = JSON.parse(tasks);
+    console.log(tasks);
+    //Print task items to the page 
+    for (var i = 0; i < tasks.length; i++) {
+        
+        taskIdCounter = tasks[i].id;
+        console.log(tasks[i]);
+        console.log(taskIdCounter);
+        var listItemEl = document.createElement("li");
+        listItemEl.className ="task-item";
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable", "true");
+        console.log(listItemEl);
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className ="task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class ='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        console.log(listItemEl);
+    }
+};
 
-//On a button click, create a task using the createTaskHandler function
+
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -301,4 +350,4 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
-
+loadTasks();
